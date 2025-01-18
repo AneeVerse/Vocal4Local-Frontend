ccd5237bc35a112b12644e24719a6d974ba93bcd
Saood Ahmad <saoodchoudhary9@gmail.com>
2025-01-18 11:13:43 +0530

    update

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -36,7 +36,7 @@ const HeroSection = () => {
 
       {/* Overlay Content */}
       <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-60 gap-[30px] flex flex-col px-4 sm:px-8 md:px-12">
-        <p className="text-white mt-[140px] sm:mt-[130px] text-sm tracking-wide mb-2">50K+ TRUSTED BUSINESSES</p>
+        <p className="text-white mt-[130px] text-sm tracking-wide mb-2">50K+ TRUSTED BUSINESSES</p>
         <h1 className="text-white text-4xl sm:text-5xl h1-lineHeight max-w-[400px] sm:max-w-[500px] md:text-6xl md:max-w-[600px]  font-bold mb-4">
           Make your <span className="italic border px-5 py-2 rounded-full inline-block decoration-white">Finance</span>  <br /> more efficient.
         </h1>
